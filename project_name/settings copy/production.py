ea76dbc8e36af344c354c224f205edd9deb204a0
Charlon Palacay <charlon@gmail.com>
2014-01-14 23:05:58 -0800

    Adding back project and settings directory.

---
+++ project_name/settings copy/production.py
@@ -0,0 +1,8 @@
+#production settings
+
+from {{ project_name }}.settings.base import *
+
+# django compressor and less-c compiler
+
+COMPRESS_OFFLINE = True
+COMPRESS_ENABLED = True
---
+++ project_name/settings/local.py
@@ -0,0 +1,8 @@
+# dev settings
+
+from {{ project_name }}.settings.base import *
+
+# django compressor and less-c compiler
+
+COMPRESS_OFFLINE = False
+COMPRESS_ENABLED = False
