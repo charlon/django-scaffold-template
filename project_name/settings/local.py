fe92548c9189a3af5881b6d9844301a746d80006
Craig M. Stimmel <cstimmel@uw.edu>
2014-02-07 12:15:19 -0800

    Added default settings for STATIC_ROOT, which has no default value in Django 1.6.2.

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -2,6 +2,10 @@
 
 from {{ project_name }}.settings.base import *
 
+# STATIC_ROOT is required as of Django 1.6.2
+# This should be a real apache served location if you're using Apache.
+STATIC_ROOT = '/tmp/'
+
 # django compressor and less-c compiler
 
 COMPRESS_OFFLINE = False
