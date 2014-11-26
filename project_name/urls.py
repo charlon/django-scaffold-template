bd5489e9df9859345c29a893e4307fb4ac4a58fa
Charlon Palacay <charlon@gmail.com>
2014-11-26 09:53:15 -0800

    Update urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -5,11 +5,13 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
+    
+    url(r'^$', 'project_name.views.example', name='example'),
+    url(r'^test/', 'project_name.views.test', name='test'),
+    
     # url(r'^blog/', include('blog.urls')),
-
     #url(r'^admin/', include(admin.site.urls)),
     
-    # include the scaffolded application
+    # include applications
     #url(r'^', include('app_name.urls')),
 )
